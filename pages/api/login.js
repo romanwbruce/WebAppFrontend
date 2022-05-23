6674a20f63cef3176a4317da84877a7e354541fb
romanwbruce <71509815+romanwbruce@users.noreply.github.com>
2022-05-22 21:49:20 -0400

    d
    d

--- pages/api/login.js
+++ pages/api/login.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 export default async function handler(req, res) {
     let username = req.query.username;
     let password = req.query.password;
-    await axios.get('http://localhost:3030/api/auth/login?username='+username+"&password="+password).then(resp => {
+    await axios.get('http://159.223.194.251/api/auth/login?username='+username+"&password="+password).then(resp => {
         res.status(200).json({response:resp.data});
         return;
     });
